6dd9f9cd1f0a2047cd9dba74e418192074b33f84
Julie <j49411@gmail.com>
2020-03-27 12:15:26 +0000

    Test: Submit button calls setCuisines function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,31 +1,33 @@
 import React from 'react';
 
-const Form = ({
-  cuisine,
-  setCuisines,
-}) => {
-  const cuisineList = cuisine.map((cuisine, key) => (
-    <option key={key} value={cuisine}>
-      {cuisine}
-    </option>
-  ))
+function Form(props) {
+  
+    const cuisineList = props.cuisine.map((cuisine, key) => (
+      <option key={key} value={cuisine}>
+        {cuisine}
+      </option>
+    ))
 
-  return (
-    <div className="app-container">
-      <select id="person_one"> 
-        {cuisineList}
-      </select>
-      <select id="person_two"> 
-        {cuisineList}
-      </select>
-      <button 
-        onClick={() => setCuisines()}
-        className="submit-button"
-      >
-        Submit!
-      </button>
-    </div>
-  );
-}
+  
+    return (
+      <div className="app-container">
+        <select id="person_one"> 
+          {cuisineList}
+        </select>
+        <select id="person_two"> 
+          {cuisineList}
+        </select>
+        <button 
+          onClick={() => props.setCuisines()}
+          className="submit-button">
+          Submit!
+        </button>
+      </div>
+    );
+
+    
+  }
+  
+
 
 export default Form;
--- src/components/Form/Form.spec.js
+++ src/components/Form/Form.spec.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Form from './Form';
 describe('Form', () => {
   let wrapper;
+  let props;
 
-  beforeEach(() =>  wrapper = shallow(
-    <Form
-      cuisine={['Chinese', 'Japanese', 'Taiwanese']}
-    />
-  ));
+  beforeEach(() =>  {
+    props = {
+      cuisine: ['Chinese', 'Japanese', 'Taiwanese'],
+      setCuisines: jest.fn()
+    };
+    wrapper = shallow(<Form {...props} />)
+  });
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
@@ -25,4 +28,13 @@ describe('Form', () => {
   it('should render submit button', () => {
     expect(wrapper.find('button').length).toEqual(1);
   });
+
+  it('should call setCuisines function when clicked on submit button', () => {
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click')
+
+    expect(props.setCuisines).toHaveBeenCalled();
+  })
 });
