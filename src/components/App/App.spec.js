a570fcd26bb986827d63d25fe6d155e96e9b1988
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-04-05 19:04:05 +0100

    Test: Default state testing

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -23,14 +23,21 @@ describe('Mounted App', () => {
 
   beforeEach(() => wrapper = mount(<App/>));
 
+  it('should have default state as Chinese for person one and two', () => {
+    expect(wrapper.state('personOneChoice')).toEqual('Chinese')
+    expect(wrapper.state('personTwoChoice')).toEqual('Chinese')
+  })
+
   it('should change the state when options is change', () => {
     wrapper.find('#person_one').simulate('change', { target: { value: 'Japanese' } })
     expect(wrapper.state('personOneChoice')).toEqual('Japanese')
   });
+
   it('should change the state when options is change', () => {
     wrapper.find('#person_two').simulate('change', { target: { value: 'Taiwanese' } })
     expect(wrapper.state('personTwoChoice')).toEqual('Taiwanese')
   });
+
 })
 
 // describe('Mounted App', () => {
